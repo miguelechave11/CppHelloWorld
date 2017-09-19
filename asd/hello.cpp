826b40665d4e9c4c34ca6333ea5dccf8ccbf5d1b
miguelechave11 <miguelechave11@gmail.com>
2017-09-19 00:11:00 -0300

    Probando

---
+++ asd/hello.cpp
@@ -0,0 +1,15 @@
+//Hello world
+//Miguel Echave
+//28/3/2017
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    
+    system("PAUSE");
+    return 0;
+}
